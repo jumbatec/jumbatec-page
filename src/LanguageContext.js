2d30ad68719a55f3308565b47fcf1ad3ae7726ce
Braimo Selimane <imolast66@gmail.com>
2024-10-25 07:53:05 +0200

    added internacionalization

--- src/LanguageContext.js
+++ src/LanguageContext.js
@@ -8,8 +8,38 @@ const LanguageProvider = ({ children }) => {
   const [lang, setLang] = useState(localStorage.getItem('lang') || 'Português');
 
   useEffect(() => {
-    localStorage.setItem('lang', lang);
-    i18next.changeLanguage(lang === "Português" ? "pt" : "en");
+
+    if(localStorage.getItem('lang') === null){
+         // Get the user's time zone
+     const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+     console.log(userTimeZone);
+     // List of time zones where Portuguese is spoken
+     const portugueseSpeakingTimeZones = [
+       "America/Sao_Paulo", // Brazil
+       "Atlantic/Cape_Verde", // Cape Verde
+       "Africa/Maputo", // Mozambique
+       "Europe/Lisbon", // Portugal
+       "Atlantic/Azores", // Portugal (Azores)
+       "Africa/Luanda", // Angola
+       "Africa/Bissau", // Guinea-Bissau
+       "Africa/Sao_Tome", // São Tomé and Príncipe
+       "Asia/Dili" // East Timor
+     ];
+ 
+     // Check if the user's time zone is in the list of Portuguese-speaking time zones
+     if (portugueseSpeakingTimeZones.includes(userTimeZone)) {
+       setLang("Português");
+     } else {
+      setLang("Enslish");
+     }
+
+     localStorage.setItem('lang', lang);
+     i18next.changeLanguage(lang === "Português" ? "pt" : "en");
+    }else{
+      localStorage.setItem('lang', lang);
+      i18next.changeLanguage(lang === "Português" ? "pt" : "en");
+    }
   }, [lang]);
  
   const changeLanguage = (newLang) => {
--- src/components/Services/service-box.js
+++ src/components/Services/service-box.js
@@ -6,7 +6,7 @@ class ServiceBox extends Component {
     return (
       <React.Fragment>
         <Col lg="4" className="mt-4">
-          <div className="services-box">
+          <div className="services-box ">
             <div className="d-flex">
               <i className={this.props.icon + " text-primary"}></i>
               <div className="ms-4">
